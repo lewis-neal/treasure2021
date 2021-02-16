5b35cc0563212979919411f3fe684eb20512fa72
Lewis Neal <l_neal_@hotmail.co.uk>
2021-02-16 00:30:02 +0000

    Add password

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import questionMark from './questionMark.png'
+import questionMark from './questionMark.png';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={questionMark}/>
-        <input type="text" placeholder="Password" className="code-text-box"></input>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {success: false};
+    this.textChange = this.textChange.bind(this);
+  }
+
+  textChange(props) {
+    if ("password" == props.target.value) {
+      this.setState({success: true});
+    }
+  }
+
+  render() {
+    let display = <input type="text" placeholder="Password" className="code-text-box" onChange={this.textChange}></input>;
+
+    if (this.state.success) {
+      display = <p className="clue">CLUE</p>;
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={questionMark}/>
+          {display}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
